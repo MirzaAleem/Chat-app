03b2dfa0b07d4fb7b3bb8689966758a30890a02c
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2024-01-26 23:58:10 +0530

    input component in messsage component is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     <div className='navbar'>
         <span className="logo">Chat App</span>
         <div className="user">
-            <img src="" alt="" />
-            <span className="userName">Aleem</span>
-            <button className="logout">logout</button>
+          <img src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*GI-td9gs8D5OKZd19mAOqA.png" alt="" />
+          <span className="userName">Aleem</span>
+          <button className="logout">logout</button>
         </div>
     </div>
   )
