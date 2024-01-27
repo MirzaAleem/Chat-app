9a6e701b27384b7c8ae289f2726e612335366a6b
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2024-01-27 22:41:27 +0530

    App Designing is completed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+        <div className="messageInfo">
+            <img src="https://media.licdn.com/dms/image/D4D03AQGNTZ13-PaumA/profile-displayphoto-shrink_800_800/0/1682182507582?e=2147483647&v=beta&t=Z6GE8JnaQw0eFCxtV4S6LkdlcI4PMWJIvG8nxvt_apU" alt="" />
+            <span>Just now</span>
+        </div>
+        <div className="messageContent">
+            <p>Hello</p>
+            <img src="https://media.licdn.com/dms/image/D4D03AQGNTZ13-PaumA/profile-displayphoto-shrink_800_800/0/1682182507582?e=2147483647&v=beta&t=Z6GE8JnaQw0eFCxtV4S6LkdlcI4PMWJIvG8nxvt_apU" alt="" />
+        </div>
+    </div>
   )
 }
 
