3431ded6c3fe1ac8a30326f3711fa7421c22576d
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2024-02-04 22:42:18 +0530

    bug fixed: using Optional chaining (?.) on a null value of  user

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -30,10 +30,10 @@ function Chats() {
   return (
     <div className='Chats'>
       {chats && Object.entries(chats)?.sort((a,b)=>{b[1].date - a[1].date}).map((chat)=>(
-        <div className="userChat" key={chat[0]} onClick={()=> handleSelect(chat[1].userInfo)}>
-          <img src={chat[1].userInfo.photoURL} alt="" />
+        <div className="userChat" key={chat[0]} onClick={()=> handleSelect(chat[1]?.userInfo)}>
+          <img src={chat[1].userInfo?.photoURL} alt="" />
           <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
+            <span>{chat[1].userInfo?.displayName}</span>
             <p>{chat[1].lastMessage?.text}</p>
           </div>
         </div>
