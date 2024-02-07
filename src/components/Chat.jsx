5f13a221e9a842335d02797a88d43fc71b34367c
Mirza Aleem Abbas <aleemabbas31@gmail.com>
2024-02-08 00:50:40 +0530

    mobile responsiveness done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,30 @@
-import React, { useContext } from 'react'
-import Cam from '../img/cam.png'
-import Add from '../img/add.png'
-import More from '../img/more.png'
-import Messages from './Messages'
-import Input from './Input'
-import { ChatContext } from '../context/chatContext'
-
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/chatContext";
 
 function chat() {
-  const { data,dispatch } = useContext(ChatContext)
+  const { data, dispatch } = useContext(ChatContext);
   const handleClick = () => {
     dispatch({ type: "CHANGE_SELECTED", payload: false });
-  }
-  console.log(data.selected)
+  };
+  console.log(data.selected);
   return (
-    <div className='chat' style={{ display: (window.innerWidth <= 450 && !data.selected) ? 'none' : 'block' }}>
+    <div
+      className="chat"
+      style={{
+        display: window.innerWidth <= 450 ? (!data.selected ? "none" : "block") : "block",
+      }}
+    >
       <div className="chatInfo">
-        <div>
-          <button onClick={handleClick}>back</button>
-          <span>{data?.user.displayName}</span>
+        <div style={{display: 'flex', alignItems:'center'}}>
+          {window.innerWidth < 450 && <span onClick={handleClick}>
+            <img src="src\img\left-chevron.png" alt="" width={'20px'}/>
+          </span>}
+          <span style={{marginLeft:'5px'}}>{data?.user.displayName}</span>
         </div>
         <div className="chatIcons">
           <img src={Cam} alt="" />
@@ -29,7 +35,7 @@ function chat() {
       <Messages />
       <Input />
     </div>
-  )
+  );
 }
 
-export default chat
+export default chat;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from '../components/Navbar'
 import Search from '../components/Search'
 import Chats from '../components/Chats'
+import { ChatContext } from '../context/chatContext'
 
 function sidebar() {
+  const { data} = useContext(ChatContext)
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{ display: window.innerWidth <= 450 ? (!data.selected ? 'block' : 'none') : 'block' }}>
       <Navbar />
       <Search />
       <Chats />
